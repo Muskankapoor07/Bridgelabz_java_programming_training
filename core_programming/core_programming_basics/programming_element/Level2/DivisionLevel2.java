90583e8db2661644125eb8e70dfdef4ee633904a
Muskankapoor07 <kapoormuskan700@gmail.com>
2026-05-04 17:04:40 +0530

    Added Level2 Java programs

---
+++ core_programming/core_programming_basics/programming_element/Level2/DivisionLevel2.java
@@ -0,0 +1,19 @@
+package core_programming.core_programming_basics.programming_element.Level2;
+
+import java.util.Scanner;
+
+class DivisionLevel2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        int quotient = a / b;
+        int remainder = a % b;
+
+        System.out.println("The Quotient is " + quotient +
+                " and Remainder is " + remainder +
+                " of two number " + a + " and " + b);
+    }
+}
