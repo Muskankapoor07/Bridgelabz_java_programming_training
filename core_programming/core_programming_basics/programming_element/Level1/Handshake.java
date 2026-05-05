0e251311d428d4c13ef1a1ec47b76463514f17c8
Muskankapoor07 <kapoormuskan700@gmail.com>
2026-05-05 15:14:16 +0530

    create code of Handshake

---
+++ core_programming/core_programming_basics/programming_element/Level1/Handshake.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Handshake {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int handshakes = (n * (n - 1)) / 2;
+
+        System.out.println("Maximum number of handshakes is " + handshakes);
+    }
+}
