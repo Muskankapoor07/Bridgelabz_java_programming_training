7598e7051a9c78f8a440ee717ba4854f2d2a7858
Muskankapoor07 <kapoormuskan700@gmail.com>
2026-05-05 15:12:08 +0530

    create code of DistanceConverter2

---
+++ core_programming/core_programming_basics/programming_element/Level1/DistanceConverter2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class DistanceConverter2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double feet = input.nextDouble();
+
+        double yards = feet / 3;
+        double miles = yards / 1760;
+
+        System.out.println("Distance in yards: " + yards +
+                " and in miles: " + miles);
+    }
+}
