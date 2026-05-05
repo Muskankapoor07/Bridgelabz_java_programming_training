aff36a8aa253194463ef24f3ef72736246ac4101
Muskankapoor07 <kapoormuskan700@gmail.com>
2026-05-05 15:40:27 +0530

    create a code of LargestDigit

---
+++ core_programming/core_programming_intermediate/Arrays/Level2/LargestDigit.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class LargestDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int largest = 0, second = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit > largest) {
+                second = largest;
+                largest = digit;
+            } else if (digit > second && digit != largest) {
+                second = digit;
+            }
+
+            num /= 10;
+        }
+
+        System.out.println("Largest = " + largest);
+        System.out.println("Second Largest = " + second);
+    }
+}
