bcbca11d56890de117d82394e7a2a051bae39fdc
Muskankapoor07 <kapoormuskan700@gmail.com>
2026-05-22 15:58:09 +0530

    DSA passengertravelanalytics

---
+++ DSA/PassengerTravelAnalytics.java
@@ -0,0 +1,14 @@
+package DSA;
+
+import java.util.*;
+
+public class PassengerTravelAnalytics {
+    public static void main(String[] args) {
+        HashMap<String, Integer> stationMap = new HashMap<>();
+        stationMap.put("Rajiv Chowk", 120);
+        stationMap.put("Kashmere Gate", 85);
+        stationMap.put("CP", 150);
+
+        System.out.println(stationMap);
+    }
+}
