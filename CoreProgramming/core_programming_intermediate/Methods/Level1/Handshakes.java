a0bec370eb0b681692ca1adbc72769b0557d9fdb
Muskan Kapoor <kapoormuskan700@gmail.com>
2026-06-15 14:59:25 +0530

    Methods

---
+++ CoreProgramming/core_programming_intermediate/Methods/Level1/Handshakes.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Handshakes {
+
+    public static int calculateHandshakes(int n) {
+
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(
+                "Enter Number of Students: ");
+
+        int students = sc.nextInt();
+
+        int handshakes =
+                calculateHandshakes(students);
+
+        System.out.println(
+                "Maximum Possible Handshakes = "
+                        + handshakes);
+    }
+}
