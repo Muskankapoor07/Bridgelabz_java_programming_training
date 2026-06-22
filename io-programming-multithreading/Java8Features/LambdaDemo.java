e0ec9b2db29c8e09448420784b59d1a5946da67d
Muskan Kapoor <kapoormuskan700@gmail.com>
2026-06-22 15:22:21 +0530

    LambdaDemo

---
+++ io-programming-multithreading/Java8Features/LambdaDemo.java
@@ -0,0 +1,10 @@
+interface Greeting {
+    void sayHello();
+}
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+        Greeting g = () -> System.out.println("Hello Java 8");
+        g.sayHello();
+    }
+}
